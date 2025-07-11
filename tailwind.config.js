201acc3973c1459a37b9909b60cc530d4876e457
Unidade Copacabana <unidade.copacabana@senacrjedu.local>
2025-07-11 13:29:29 -0300

    iosfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {
+        screens: {
+          'fix': '405px',
+        },
         fontFamily: {
           poppins: ['Poppins', 'sans-serif'],
           inter: ['Inter', 'sans-serif'],
