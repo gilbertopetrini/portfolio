e8e88bec5f2a6f3bf96acdecb711ca8c88f66b78
gilbertopetrini <cronosizinho@gmail.com>
2025-07-12 16:02:04 -0300

    media query changes

--- app/componentes/Projetos/props.jsx
+++ app/componentes/Projetos/props.jsx
@@ -4,12 +4,12 @@ import Styles from "./Projetos.module.css";
 export default function ProjetosProps(props) {
     return(
     <div className={Styles.projeto}>
+        <h2>{props.titulo}</h2>
         <div className={Styles.projetoimg}>
             <Link href={props.href}><img src={props.imagem} className={Styles.imghover}></img></Link>
         </div>
 
         <div className={Styles.projetodesc}>
-            <h2>{props.titulo}</h2>
             <p>{props.descricao}</p>
         </div>
     </div>
