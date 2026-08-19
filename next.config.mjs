39d47a1185b302b8750450819025e2171e759093
petrinidias <petrini4u@gmail.com>
2026-08-19 16:00:44 -0300

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'ptrn.vercel.app',
+          },
+        ],
+        destination: 'https://ptrn.com.br/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 export default nextConfig;
